d13ae2bba68cace3f128c8db8965e06b3e7c8ffb
chdeskur <chdeskur@gmail.com>
2021-01-08 23:58:10 -0500

    Updates

---
+++ precache-manifest.ff3b2fae127c6779e5069aa5d07c1196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fadcf58e63e3f01efb981fe8c2f9e43",
+    "url": "/chdeskur.github.io/index.html"
+  },
+  {
+    "revision": "bd867b9c18e0b5abd691",
+    "url": "/chdeskur.github.io/static/js/2.49086dd8.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/chdeskur.github.io/static/js/2.49086dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532a5698a84bdb69ee51",
+    "url": "/chdeskur.github.io/static/js/main.813d5f4c.chunk.js"
+  },
+  {
+    "revision": "bb02ba3627ca6afae67e",
+    "url": "/chdeskur.github.io/static/js/runtime-main.9ce425c7.js"
+  }
+]);
